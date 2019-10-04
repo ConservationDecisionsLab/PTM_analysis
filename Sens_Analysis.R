55a9c50ae7a83a41df889a75a83da502cf3d27a2
aecamaclang <abbey.camaclang@gmail.com>
2019-10-04 16:31:20 -0700

    New lines to calculate values for new 'All Strategy' combinations, and for calculating expected performance for each iteration and saving as .rds object for calling up in the optimization as the benefits matrix

--- Sens_Analysis.R
+++ Sens_Analysis.R
@@ -24,7 +24,8 @@ library(mc2d)
 library(tidyverse)
 library(cowplot)
 
-MC <-  10000 # number of iterations
+MC <-  100 # number of iterations
+a <- 1000000 # scaling for CE scores
 
 #' ## Code copied from createBoxplots.R, with minimal changes to accommodate the absence of some tables in the Environment
 
@@ -72,9 +73,33 @@ costfeas <- costfeas[-1,] # Remove baseline values
 # costfeas$Strategy <- as_factor(costfeas$Strategy)
 costfeas$Strategy <- factor(costfeas$Strategy, levels = strat.levels)
 
+# Calculate cost and feasibility values for new combinations --------------
+
+combos <- as.character(costfeas$Strategy[17:length(costfeas$Strategy)])
+
+S22.exc <- c("Baseline", "S6", combos)
+S22.feas <- mean(costfeas$Feasibility[!costfeas$Strategy %in% S22.exc])
+S22.cost <- costfeas$Cost[which(costfeas$Strategy == "S22")] - costfeas$Cost[which(costfeas$Strategy == "S6")]
+
+S23.exc <- c("Baseline", "S5", combos)
+S23.feas <- mean(costfeas$Feasibility[!costfeas$Strategy %in% S23.exc])
+S23.cost <- costfeas$Cost[which(costfeas$Strategy == "S22")] - costfeas$Cost[which(costfeas$Strategy == "S5")]
+
+# Add new cost/feasibility values to costfeas:
+costfeas <- costfeas %>%
+  add_row(Strategy = "S23", Cost = S23.cost, Feasibility = S23.feas)
+costfeas$Cost[which(costfeas$Strategy == "S22")] <- S22.cost
+costfeas$Feasibility[which(costfeas$Strategy == "S22")] <- S22.feas
+
+# get new strat.levels
+newstrat.levels <- levels(costfeas$Strategy)
+
+# comment out section if not needed -----------------------------------------------
+
 #' ## Do uncertainty analysis
 samples <- matrix(nrow = nrow(rlong.std.wide),ncol = MC)
 MC.CE_Score <- list()
+Benefits_uncrtn <- list()
 
 # testsample <- subset(rlong.std.wide, Strategy == "Baseline" | Strategy == "S12")
 # tsamples <- matrix(nrow = nrow(testsample), ncol = MC)
@@ -86,18 +111,16 @@ for(it in 1:MC){
                         min = rlong.std.wide$Lower,
                         mode = rlong.std.wide$Best.Guess,
                         max = rlong.std.wide$Upper, shape=4)
-  # tsamples[,it] <- rpert(nrow(testsample), 
-  #                       min = testsample$Lower, 
-  #                       mode = testsample$Best.Guess, 
-  #                       max = testsample$Upper, shape=4)
+  # samples[,it] <- rlong.std.wide$Best.Guess
   
   temp <- cbind(rlong.std.wide[,1:3], samples[,it])
   names(temp)[4] <- "MC.Value"
   
   temp.wide <- spread(temp, key = Strategy, value = MC.Value)
-  temp.strat <- select(temp.wide, S1:S22)
-  # temp.strat <- select(temp.wide, S12)
+  temp.strat <- select(temp.wide, as.character(strat.levels[2]):as.character(strat.levels[length(strat.levels)])) #select(temp.wide, S1:S22)
+  
   temp.ben <- temp.strat - temp.wide$Baseline
+  # temp.ben[temp.ben<0] <- 0 # replaces negative values with 0 (assume same as baseline)
   temp.ben <- cbind(temp.wide[,1:2], temp.ben) 
   
   temp.long <- gather(temp.ben, key = Strategy, value = MCValue, -c(1:2))
@@ -121,22 +144,52 @@ for(it in 1:MC){
   temp.agg <- aggregate(temp.joined$Wt.MCValue, 
                         by = list(Ecological.Group = temp.joined$Ecological.Group, Strategy = temp.joined$Strategy), 
                         FUN = sum, na.rm = TRUE)
-  
-  #' Weight by number of species in group
+
+  #' Weight benefits by number of species in group
   wt.temp.agg <- left_join(temp.agg, grpwts, by = "Ecological.Group") %>%
-    mutate(Wt.Benefit = x*numspp)
+    mutate(Wt.Benefit = x*numspp)    
+ 
   
-  # Add up (unweighted) aggregated benefits of each strategy across ecological groups
+  # Calculate benefit values for new combinations -----------------------------------
+  # turn off if not needed
+  
+  S6.fishben <- wt.temp.agg$Wt.Benefit[wt.temp.agg$Ecological.Group == "Migratory Fish" & wt.temp.agg$Strategy== "S6"]
+  S5.fishben <- wt.temp.agg$Wt.Benefit[wt.temp.agg$Ecological.Group == "Migratory Fish" & wt.temp.agg$Strategy== "S5"]
+  SAll.fishben <- wt.temp.agg$Wt.Benefit[wt.temp.agg$Ecological.Group == "Migratory Fish" & wt.temp.agg$Strategy== "S22"]
+  
+  S6.wt.fishben <- S6.fishben * costfeas$Feasibility[which(costfeas$Strategy == "S6")]
+  S5.wt.fishben <- S5.fishben * costfeas$Feasibility[which(costfeas$Strategy == "S5")]
+  SAll.wt.fishben <- SAll.fishben * costfeas$Feasibility[which(costfeas$Strategy == "S22")]
+  
+  S22.wtben <- SAll.wt.fishben - (S6.wt.fishben - S5.wt.fishben)
+  S22.ben <- S22.wtben/costfeas$Feasibility[which(costfeas$Strategy == "S22")] # unweight
+  
+  # Copy old S22 values as S23:
+  S23 <- wt.temp.agg[which(wt.temp.agg$Strategy == "S22"),]
+  S23$Strategy <- "S23"
+  rownames(S23) <- c(199:(199+8))
+  
+  # Replace old S22 migratory fish values with new ones, then add new S23 values
+  wt.temp.agg$Wt.Benefit[which(wt.temp.agg$Strategy=="S22" & wt.temp.agg$Ecological.Group=="Migratory Fish")] <- S22.ben
+  fish.numspp <- wt.temp.agg$numspp[which(wt.temp.agg$Strategy=="S22" & wt.temp.agg$Ecological.Group=="Migratory Fish")]
+  wt.temp.agg$x[which(wt.temp.agg$Strategy=="S22" & wt.temp.agg$Ecological.Group=="Migratory Fish")] <- S22.ben/fish.numspp
+  wt.temp.agg <- rbind(wt.temp.agg, S23) # new values (unweighted by feasibility) one for each ecol group and strategy. Use for getting new benefit matri
+  
+  # strat.levels <- newstrat.levels
+
+  # comment out above section if not needed ---------------------------------------
+  
+  # Add up group-weighted aggregated benefits of each strategy across ecological groups
   # first rearrange so strategies are in rows and ecol groups are in columns - basically need to end up with col vector of total benefit
   sum.ben <- aggregate(wt.temp.agg$Wt.Benefit, 
                        by = list(Strategy = wt.temp.agg$Strategy),
                        FUN = sum, na.rm = TRUE)
   names(sum.ben) <- c("Strategy", "Benefit")
-  sum.ben$Strategy <- factor(sum.ben$Strategy, levels = strat.levels)
+  sum.ben$Strategy <- as_factor(sum.ben$Strategy)
   
   # Join with cost/feasibility table and calculate cost-effectiveness
   strat.est <- left_join(sum.ben, costfeas, by="Strategy") %>%
-    mutate(., Sc.Cost = Cost/1000000, # scale costs to get reasonable values
+    mutate(., Sc.Cost = Cost/a, # scale costs to get reasonable values
            Exp.Benefit = Benefit * Feasibility, # weight benefits
            CE = (Benefit * Feasibility)/Sc.Cost) # calculate cost-effectiveness scores
   
@@ -147,21 +200,59 @@ for(it in 1:MC){
            Cost_rank = rank(Cost))
   
   MC.CE_Score[[it]] <- CE_Score
+  
+  #' Calculate expected performance for use as Benefit matrix in optimization
+  # Get unweighted aggregated benefit estimates and weight by feasibility
+  temp.joined.data <- left_join(wt.temp.agg, costfeas, by = "Strategy") %>%
+    mutate(., Wt.Value = x * Feasibility) # weight by feasibility
+  wide.temp.agg <- select(temp.joined.data, Ecological.Group, Strategy, Wt.Value) %>%
+    spread(Strategy, Wt.Value)
+  
+  # Aggregate baseline
+  temp.base <- temp.wide[,1:3]
+  temp.base$Strategy <- "Baseline"
+  names(temp.base)[3] <- "MCValue"
+  temp.base.joined <- left_join(temp.base, spcases, by=c("Expert", "Ecological.Group", "Strategy")) %>%
+    left_join(., grpwts, by = "Ecological.Group")
+  fullwts.idx.base <- which(is.na(temp.base.joined$NumSppScored))
+  temp.base.joined$NumSppScored[fullwts.idx.base] <- temp.base.joined$numspp[fullwts.idx.base]
+  
+  fullwts.base <- aggregate(temp.base.joined$NumSppScored, 
+                            by = list(Ecological.Group = temp.base.joined$Ecological.Group, Strategy = temp.base.joined$Strategy), 
+                            FUN = sum, na.rm = TRUE)
+  temp.base.joined <- temp.base.joined %>%
+    left_join(., fullwts.base, by = c("Ecological.Group", "Strategy")) %>%
+    mutate(Wts = NumSppScored/x) %>%
+    mutate(Wt.MCValue = MCValue*Wts)
+  
+  temp.base.agg <- aggregate(temp.base.joined$Wt.MCValue, 
+                             by = list(Ecological.Group = temp.base.joined$Ecological.Group, Strategy = temp.base.joined$Strategy), 
+                             FUN = sum, na.rm = TRUE)
+  names(temp.base.agg)[3] <- "Wt.Best.guess"
+  
+  # Calculate performance and save in a list
+  wt.perf <- wide.temp.agg[,2:ncol(wide.temp.agg)] + as.matrix(temp.base.agg[,3])
+  Benefits_uncrtn[[it]] <- wt.perf %>%
+    add_column(Ecological.Group = wide.temp.agg$Ecological.Group, .before = "S1")
+  
 }  
 
-MC.CE_Table <- lapply(MC.CE_Score, "[", 1:22, "CE")
+MC.CE_Table <- lapply(MC.CE_Score, "[", 1:length(strat.est$Strategy), "CE")
 MC.Results <- matrix(unlist(MC.CE_Table), ncol = MC, byrow = FALSE)
-MC.Results <- data.frame(strat.levels[2:length(strat.levels)], MC.Results)
+MC.Results <- data.frame(strat.est$Strategy[1:length(strat.est$Strategy)], MC.Results)
 names(MC.Results)[1] <- "Strategy"
 
 #' Save results
 write_csv(MC.Results, "MC_CEScores2.csv")
 MC_Samples <- data.frame(rlong.std.wide[,1:3], samples)
 write_csv(MC_Samples, "MC_PerfSamples2.csv")
+saveRDS(Benefits_uncrtn, "Benefits_uncrtn.rds")
 
 #' Box plots for visualization
 MC.CE <- gather(MC.Results, key = MC.Iter, value = CE, 2:ncol(MC.Results))
-MC.CE$Strategy <- factor(MC.CE$Strategy, levels = strat.levels)
+# MC.CE$Strategy <- factor(MC.CE$Strategy, levels = unique(MC.CE$Strategy))
+
+strat.levels <- newstrat.levels
 
 temp.plot <-
   ggplot(MC.CE, aes(x = Strategy, # for each Ecological group, plot Estimate Type on x-axis 
@@ -180,7 +271,7 @@ temp.plot <-
         plot.caption = element_text(size = 10, hjust = 0)
   ) + 
   # facet_wrap( ~ Strategy, nrow = 3) +  # create a separate panel of estimates for each management strategy
-  scale_x_discrete(name = "Management Strategies",
+  scale_x_discrete(name = "Management strategies",
                    breaks = strat.levels[2:length(strat.levels)],
                    labels = strat.levels[2:length(strat.levels)]# Give the x-axis variables labels
   ) + 
